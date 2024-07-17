f51ae5eb30fd867aa99120ab52fa45eaa0d48bb2
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-17 23:45:24 +0200

    Declare separate pointcut

--- src/main/kotlin/com/iurii/aopdemo/aspect/DemoLoggingAspect.kt
+++ src/main/kotlin/com/iurii/aopdemo/aspect/DemoLoggingAspect.kt
@@ -2,13 +2,17 @@ package com.iurii.aopdemo.aspect
 
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
+import org.aspectj.lang.annotation.Pointcut
 import org.springframework.stereotype.Component
 
 @Aspect
 @Component
 class DemoLoggingAspect {
 
-    @Before("execution(* com.iurii.aopdemo.dao.account.*.*(..))")
+    @Pointcut("execution(* com.iurii.aopdemo.dao.account.*.*(..))")
+    fun forDAOPackage() { }
+
+    @Before("forDAOPackage()")
     fun beforeAddAccountAdvice() {
         println("=======> Executing @Before advice on addAccount method")
     }
