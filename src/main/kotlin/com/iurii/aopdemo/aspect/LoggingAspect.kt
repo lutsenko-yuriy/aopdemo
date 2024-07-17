41d33b8f0c35a80f6c4a1001eb6bd869c5c8533e
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-18 00:28:02 +0200

    Fetch arguments from methods for advices

--- src/main/kotlin/com/iurii/aopdemo/aspect/LoggingAspect.kt
+++ src/main/kotlin/com/iurii/aopdemo/aspect/LoggingAspect.kt
@@ -1,7 +1,9 @@
 package com.iurii.aopdemo.aspect
 
+import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
+import org.aspectj.lang.reflect.MethodSignature
 import org.springframework.stereotype.Component
 
 @Aspect
@@ -9,8 +11,18 @@ import org.springframework.stereotype.Component
 class LoggingAspect {
 
     @Before("com.iurii.aopdemo.aspect.AopExpressions.forDAOPackage()")
-    fun beforeAddAccountAdvice() {
+    fun beforeAddAccountAdvice(joinPoint: JoinPoint) {
         println("=======> Executing @Before advice on addAccount method")
+
+        val signature = joinPoint.signature as MethodSignature
+
+        println("Method $signature")
+
+        val args = joinPoint.args
+
+        for ((index, arg) in args.withIndex()) {
+            println("Argument ${index}: $arg")
+        }
     }
 
 }
