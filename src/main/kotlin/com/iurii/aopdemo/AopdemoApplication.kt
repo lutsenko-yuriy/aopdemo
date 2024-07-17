3f8f1fcc8c0746e96537b6677330bf94a868adb8
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-17 21:38:16 +0200

    Add necessary dependencies

--- src/main/kotlin/com/iurii/aopdemo/AopdemoApplication.kt
+++ src/main/kotlin/com/iurii/aopdemo/AopdemoApplication.kt
@@ -1,10 +1,20 @@
 package com.iurii.aopdemo
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class AopdemoApplication
+class AopdemoApplication {
+
+    @Bean
+    fun commandLineRunner(): CommandLineRunner {
+        return CommandLineRunner {
+            println("Hello World")
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<AopdemoApplication>(*args)
