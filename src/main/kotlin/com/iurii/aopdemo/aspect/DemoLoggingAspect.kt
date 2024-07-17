84d34bd352a5fbca91fdb53083d0b3104ddafcf0
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-17 21:45:35 +0200

    Add LoggingAspect

---
+++ src/main/kotlin/com/iurii/aopdemo/aspect/DemoLoggingAspect.kt
@@ -0,0 +1,15 @@
+package com.iurii.aopdemo.aspect
+
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
+import org.springframework.stereotype.Component
+
+@Aspect
+@Component
+class DemoLoggingAspect {
+
+    @Before("execution(public void addAccount())")
+    fun beforeAddAccountAdvice() {
+        println("=======> Executing @Before advice on addAccount method")
+    }
+}
