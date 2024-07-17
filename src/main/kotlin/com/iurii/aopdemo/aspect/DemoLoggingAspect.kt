f6224ee4359773e0cc9ed6fa0309443307bd6c9c
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-17 23:48:22 +0200

    Add analytics

--- src/main/kotlin/com/iurii/aopdemo/aspect/DemoLoggingAspect.kt
+++ src/main/kotlin/com/iurii/aopdemo/aspect/DemoLoggingAspect.kt
@@ -4,10 +4,13 @@ import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
 import org.aspectj.lang.annotation.Pointcut
 import org.springframework.stereotype.Component
+import java.util.concurrent.atomic.AtomicInteger
 
 @Aspect
 @Component
 class DemoLoggingAspect {
+
+    val calledTimes = AtomicInteger()
 
     @Pointcut("execution(* com.iurii.aopdemo.dao.account.*.*(..))")
     fun forDAOPackage() { }
@@ -15,5 +18,10 @@ class DemoLoggingAspect {
     @Before("forDAOPackage()")
     fun beforeAddAccountAdvice() {
         println("=======> Executing @Before advice on addAccount method")
+    }
+
+    @Before("forDAOPackage()")
+    fun performAPIAnalytics() {
+        println("=======> Called ${calledTimes.incrementAndGet()} times")
     }
 }
